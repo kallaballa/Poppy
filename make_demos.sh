5721e75b72aeda8b78d02f7410fa74206115b95e
kallaballa <amir@viel-zu.org>
2022-03-05 19:48:36 +0100

    updated demos

--- make_demos.sh
+++ make_demos.sh
@@ -14,7 +14,7 @@ src/poppy -o cars.mkv -f60 kindpng1s.png kindpng2s.png kindpng1s.png
 mencoder -vf scale=250:160 -ovc x264 -nosound -fps 30 -ofps 6 cars.mkv -o low.mp4
 ffmpeg -y -i low.mp4 demo/cars.gif
 
-src/poppy -o browns.mkv -l1 -a1 -f60 amir1.jpg amir2.jpg amir1.jpg
+src/poppy -o browns.mkv -s 1.3 -l1 -a1 -f60 amir1.jpg amir2.jpg amir1.jpg
 mencoder -vf scale=360:202 -ovc x264 -nosound -fps 30 -ofps 6 browns.mkv -o low.mp4
 ffmpeg -y -i low.mp4 demo/browns.gif
 
