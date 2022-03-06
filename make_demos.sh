87bac218c55a95e069d2055e715626529e8b3011
kallaballa <amir@viel-zu.org>
2022-03-06 08:59:05 +0100

    fine tuning

--- make_demos.sh
+++ make_demos.sh
@@ -6,35 +6,35 @@ LOW=`mktemp --suffix=.mp4`
 mkdir -p ../videos/
 rm ../videos/*mkv
 
-../src/poppy -o ../videos/faces.mkv -f60 some1.png some2.png some3.png some1.png
+../src/poppy -o ../videos/faces.mkv some1.png some2.png some3.png some1.png
 mencoder -vf scale=250:320 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/faces.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/faces.gif
 
-../src/poppy -o ../videos/cars.mkv -f60 kindpng1s.png kindpng2s.png kindpng1s.png
+../src/poppy -o ../videos/cars.mkv kindpng1s.png kindpng2s.png kindpng1s.png
 mencoder -vf scale=250:160 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/cars.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/cars.gif
 
-../src/poppy -o ../videos/browns.mkv -s 1.3 -f60 amir1.jpg amir2.jpg amir1.jpg
+../src/poppy -o ../videos/browns.mkv amir1.jpg amir2.jpg amir1.jpg
 mencoder -vf scale=360:202 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/browns.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/browns.gif
 
-../src/poppy -o ../videos/catdog.mkv -c1.5 -s2 -f60 cat.png dog.png cat.png
+../src/poppy -o ../videos/catdog.mkv -c1.5 -s2 cat.png dog.png cat.png
 mencoder -vf scale=320:320 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/catdog.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/catdog.gif
 
-../src/poppy -o ../videos/squarecircle.mkv -s8 -f60 circle.png square.png circle.png
+../src/poppy -o ../videos/squarecircle.mkv -s8 circle.png square.png circle.png
 mencoder -vf scale=320:320 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/squarecircle.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/squarecircle.gif
 
-../src/poppy -o ../videos/numbers.mkv -s10 -f60 1st.png 2nd.png 3rd.png 2nd.png 1st.png
+../src/poppy -o ../videos/numbers.mkv -s10 1st.png 2nd.png 3rd.png 2nd.png 1st.png
 mencoder -vf scale=320:320 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/numbers.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/numbers.gif
 
-../src/poppy -o ../videos/flowers.mkv -s1.5 -f60 flower1.jpg flower2.jpg flower1.jpg
+../src/poppy -o ../videos/flowers.mkv -s1.5 flower1.jpg flower2.jpg flower1.jpg
 mencoder -vf scale=320:320 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/flowers.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/flowers.gif
 
-../src/poppy -o ../videos/yalefaces.mkv -f60 subject01*.png subject01.glasses.png
+../src/poppy -o ../videos/yalefaces.mkv subject01*.png subject01.glasses.png
 mencoder -vf scale=320:320 -ovc lavc -lavcopts vcodec=ffv1 -nosound -fps 30 -ofps 6 ../videos/yalefaces.mkv -o "$LOW"
 ffmpeg -y -i "$LOW" ../demo/yalefaces.gif
 
